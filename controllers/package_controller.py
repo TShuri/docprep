1348cc3b0c8f9a8db36e41e541e141b1163e8ff5
TShuri <andr.xti@gmail.com>
2025-08-20 10:11:02 +0800

    Удаляются лишние абзацы и слова

--- controllers/package_controller.py
+++ controllers/package_controller.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from core import docx_tools, file_tools
 from utils.settings_utils import load_bank_requisites_directory, load_work_directory
-from utils.templates_utils import load_del_words
+from utils.templates_utils import load_del_paragraphs, load_del_words
 from utils.text_utils import get_case_number_from_filename, sanitize_filename
 
 
@@ -18,8 +18,7 @@ class PackageController:
         self.current_path_dossier = None  # Путь к папке досье
         self.have_bank_requisites = False  # Флаг наличия реквизитов банков
         self.update_bank_requisites()
-        
-        
+
     def update_bank_requisites(self):
         path_requirities = load_bank_requisites_directory()
         if not path_requirities:
@@ -29,67 +28,71 @@ class PackageController:
             doc_requirities = docx_tools.open_docx(path_requirities)
             banks = docx_tools.get_bank_list(doc_requirities)
             self.view.set_bank_list(banks)
-            
+
     def handle_checkbox_toggled(self, state):
         enabled = state == 0  # 0 = unchecked, 2 = checked
-        self.view.btn_insert_statement.setVisible(not enabled)   # видимость
-        self.view.btn_insert_statement.setEnabled(not enabled)   # активность
-        
+        self.view.btn_insert_statement.setVisible(not enabled)  # видимость
+        self.view.btn_insert_statement.setEnabled(not enabled)  # активность
+
     def handle_reset_clicked(self):
         self.view.reset_bank()
         self.view.reset()
         self.current_path_doc = None
         self.current_path_dossier = None
-        
-        
+
     def handle_process_clicked(self):
         self.view.reset()
         self.current_path_doc = None
         self.current_path_dossier = None
         folder_path = Path(load_work_directory())
-        
+
         if not folder_path.exists() or not folder_path.is_dir():
             self.view.append_log('Пожалуйста, укажите путь к рабочей папке.')
             return
-        
+
         if self.view.checkbox_statement.isChecked():
-            self._form_package_without_statement(folder_path)  # Формирование пакета документов без заявления
+            self._form_package_without_statement(
+                folder_path
+            )  # Формирование пакета документов без заявления
             return
-        
+
         if self.view.bank_selector.currentText() == '— выберите банк —':
             self.view.append_log('Выберите банк перед продолжением.')
             return
 
-        self._form_package(folder_path) # Формирование пакета документов
+        self._form_package(folder_path)  # Формирование пакета документов
         if not self.current_path_doc:
             self.view.append_log('Не удалось найти документ РТК в указанной папке.')
             return
-        self._proccess_statement(self.current_path_doc)  # Обработка заявления на включение требований в реестр
-        
+        self._proccess_statement(
+            self.current_path_doc
+        )  # Обработка заявления на включение требований в реестр
+
         self.view.append_log('Пакет документов сформирован.')
-        
-    def handle_insert_statement(self):        
+
+    def handle_insert_statement(self):
         if self.view.bank_selector.currentText() == '— выберите банк —':
             self.view.append_log('Выберите банк перед продолжением.')
             return
-        
+
         self.view.reset()
         self.current_path_doc = None
         folder_path = Path(load_work_directory())
-        
+
         if not folder_path.exists() or not folder_path.is_dir():
             self.view.append_log('Пожалуйста, укажите путь к рабочей папке.')
             return
-        
+
         self._insert_statement(folder_path)  # Вставка заявления в пакет документов
         if not self.current_path_doc:
             self.view.append_log('Не удалось найти Заявление в указанной папке.')
             return
-        self._proccess_statement(self.current_path_doc)  # Обработка заявления на включение требований в реестр
-        
+        self._proccess_statement(
+            self.current_path_doc
+        )  # Обработка заявления на включение требований в реестр
+
         self.view.append_log('Пакет документов сформирован.')
-        
-        
+
     def _form_package(self, folder_path: str):
         """Формирование пакета документов по банкротству"""
         folder = Path(folder_path)
@@ -103,8 +106,12 @@ class PackageController:
             # 2️⃣ Разархивировать досье
             path_archive = file_tools.find_dossier_archive(folder)  # Путь к архиву досье
             path_extract = folder / fio_debtor  # Папка для распаковки архива досье
-            path_dossier = file_tools.unzip_archive(path_archive, path_extract)  # Распаковка архива досье
-            file_tools.unzip_all_nested_archives(path_dossier)  # Распаковка вложенных архивов в досье
+            path_dossier = file_tools.unzip_archive(
+                path_archive, path_extract
+            )  # Распаковка архива досье
+            file_tools.unzip_all_nested_archives(
+                path_dossier
+            )  # Распаковка вложенных архивов в досье
             file_tools.delete_file(path_archive)  # Удаление архива досье после распаковки
 
             # 3️⃣ Переместить документ РТК в папку досье
@@ -116,7 +123,9 @@ class PackageController:
 
             # 5️⃣ Скопировать папки обязательств в папку арбитражного дела
             # Исключая папку арбитражного дела, если она уже существует
-            paths_obligations = file_tools.find_folders_obligations(path_dossier)  # Поиск папок обязательств
+            paths_obligations = file_tools.find_folders_obligations(
+                path_dossier
+            )  # Поиск папок обязательств
             for path_oblig in paths_obligations:
                 if path_oblig == path_arbitter:
                     continue
@@ -126,25 +135,32 @@ class PackageController:
 
         except Exception as e:
             self.view.append_log(f'Произошла ошибка: {e}')
-            
+
     def _form_package_without_statement(self, folder_path: str):
         """Формирование пакета документов без заявления"""
         folder = Path(folder_path)
         try:
             path_archive = file_tools.find_dossier_archive(folder)  # Путь к архиву досье
-            case_number = get_case_number_from_filename(path_archive.name)  # Извлечение номера дела из имени архива
-            path_extract = f'{folder / case_number} без заявления'  # Папка для распаковки архива досье
-            path_dossier = file_tools.unzip_archive(path_archive, path_extract)  # Распаковка архива досье
+            case_number = get_case_number_from_filename(
+                path_archive.name
+            )  # Извлечение номера дела из имени архива
+            path_extract = (
+                f'{folder / case_number} без заявления'  # Папка для распаковки архива досье
+            )
+            path_dossier = file_tools.unzip_archive(
+                path_archive, path_extract
+            )  # Распаковка архива досье
             self.current_path_dossier = path_dossier  # Сохраняем путь к папке досье
-            file_tools.unzip_all_nested_archives(path_dossier)  # Распаковка вложенных архивов в досье
+            file_tools.unzip_all_nested_archives(
+                path_dossier
+            )  # Распаковка вложенных архивов в досье
             file_tools.delete_file(path_archive)  # Удаление архива досье после распаковки
-            
+
             self.view.set_current_case(f'{case_number} без заявления')
             self.view.append_log('Пакет документов сформирован без заявления.')
         except Exception as e:
             self.view.append_log(f'Произошла ошибка: {e}')
-                
-    
+
     def _proccess_statement(self, path_doc: Path) -> None:
         """Обработка заявления"""
         doc = docx_tools.open_docx(path_doc)
@@ -154,12 +170,17 @@ class PackageController:
                 func(*args)
                 doc.save(path_doc)
             except Exception as e:
-                self.view.append_log(f"{step_name}: ошибка — {e}")
-                
+                self.view.append_log(f'{step_name}: ошибка — {e}')
+
         # Удаление слов из документа
         del_words = load_del_words()
         if del_words:
-            _step('Удаление слов', docx_tools.delete_words, doc, load_del_words())
+            _step('Удаление слов', docx_tools.delete_words, doc, del_words)
+
+        # Удаление параграфов из документа
+        del_paragraphs = load_del_paragraphs()
+        if del_paragraphs:
+            _step('Удаление параграфов', docx_tools.delete_paragraphs, doc, del_paragraphs)
 
         # Форматирование списка приложений
         _step('Форматирование приложений', docx_tools.format_appendices, doc)
@@ -168,13 +189,16 @@ class PackageController:
         if self.have_bank_requisites:
             path_requisites = load_bank_requisites_directory()
             doc_requisities = docx_tools.open_docx(path_requisites)
-            _step('Вставка реквизитов банка',
-                    docx_tools.insert_bank_table,
-                    doc, doc_requisities, self.view.bank_selector.currentText()
-                )
+            _step(
+                'Вставка реквизитов банка',
+                docx_tools.insert_bank_table,
+                doc,
+                doc_requisities,
+                self.view.bank_selector.currentText(),
+            )
         else:
             self.view.append_log('Банковские реквизиты не заменены')
-            
+
     def _insert_statement(self, folder_path: str) -> None:
         """Вставка заявления в пакет документов"""
         folder = Path(folder_path)
@@ -184,11 +208,13 @@ class PackageController:
             doc = docx_tools.open_docx(path_doc)  # Открытие документа РТК
             fio_debtor = docx_tools.extract_fio_debtor(doc)  # Извлечение ФИО должника
             case_number = docx_tools.extract_case_number(doc)  # Извлечение номера дела
-            
+
             # 2️⃣ Поиск и переименование папки досье
             path_dossier = self.current_path_dossier
-            path_dossier = file_tools.rename_folder(path_dossier, fio_debtor)  # Переименование папки досье
-            
+            path_dossier = file_tools.rename_folder(
+                path_dossier, fio_debtor
+            )  # Переименование папки досье
+
             # 3️⃣ Переместить документ РТК в папку досье
             self.current_path_doc = file_tools.move_file(path_doc, path_dossier)
 
@@ -198,7 +224,9 @@ class PackageController:
 
             # 5️⃣ Скопировать папки обязательств в папку арбитражного дела
             # Исключая папку арбитражного дела, если она уже существует
-            paths_obligations = file_tools.find_folders_obligations(path_dossier)  # Поиск папок обязательств
+            paths_obligations = file_tools.find_folders_obligations(
+                path_dossier
+            )  # Поиск папок обязательств
             for path_oblig in paths_obligations:
                 if path_oblig == path_arbitter:
                     continue
@@ -206,4 +234,4 @@ class PackageController:
 
             self.view.set_current_case(f'{case_number} {fio_debtor}')
         except Exception as e:
-            self.view.append_log(f"Ошибка при добавлении заявления: {e}")
+            self.view.append_log(f'Ошибка при добавлении заявления: {e}')
--- utils/templates_utils.py
+++ utils/templates_utils.py
@@ -1,5 +1,6 @@
 DEL_WORDS_PATH = 'templates/del_words.txt'
 
+
 def load_del_words() -> list[str]:
     """Загрузка слов для удаления из файла"""
     try:
@@ -7,9 +8,23 @@ def load_del_words() -> list[str]:
             words = [line.strip('\n') for line in file if line.strip()]
         return words
     except FileNotFoundError:
-        print(f"Файл {DEL_WORDS_PATH} не найден.")
-        
-if __name__ == "__main__":
+        print(f'Файл {DEL_WORDS_PATH} не найден.')
+
+
+DEL_PARAGRAPHS_PATH = 'templates/del_paragraphs.txt'
+
+
+def load_del_paragraphs() -> list[str]:
+    """Загрузка параграфов для удаления из файла"""
+    try:
+        with open(DEL_PARAGRAPHS_PATH, 'r', encoding='utf-8') as file:
+            paragraphs = [line.strip('\n') for line in file if line.strip()]
+        return paragraphs
+    except FileNotFoundError:
+        print(f'Файл {DEL_PARAGRAPHS_PATH} не найден.')
+
+
+if __name__ == '__main__':
     # Пример использования
     words = load_del_words()
     print(words)
